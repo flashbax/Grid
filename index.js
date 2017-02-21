7fa09dc3cae5f3731572fbf899ebc1073b094557
flashbax <jkiwtco@gmail.com>
2017-02-21 16:49:17 -0500

    Commit 6

--- index.js
+++ index.js
@@ -1,92 +1,62 @@
 window.onload = function() {
     
+    var body = document.getElementsByTagName('body');
+    //document.createElement('div').setAttribute('id', 'container');
+    
     var blue = colourBlue('#4286f4');
     var red = colourRed('#c40909');
     var green = colourGreen('#62f441');
     //add array
-    arr = [blue, red, green];
-    
-    //add rows of divs to page with id
-    function addRowDiv() { 
-        for (var i = 0; i<=2; i++) { 
-            var divRow = document.createElement('div'); 
-            divRow.setAttribute('id', 'row'+[i]);
-            document.body.appendChild(divRow);
-        }    
-    };
+    arr = [blue, red, green]
 
-    //add columns with id's 1-3
-    function addColDivRow1() { 
-        
-        
-        //var randommath = Math.floor(Math.random() * 3);
-        for (var i = 0; i<=2; i++) { 
+
+    function addColDiv() {     
+        for (var i = 0; i<=8; i++) { 
             var divCol = document.createElement('div'); 
-            divCol.setAttribute('id', 'col'+[i]);
-            document.getElementById('row0').appendChild(divCol);
+            divCol.setAttribute('id', 'col'+i);
+            document.getElementById('body').appendChild(divCol);
             document.getElementById('col'+i).style.backgroundColor = arr[Math.floor(Math.random() * 3)];
-        }
+            console.log(arr);
+        };
     };
-    //add columns with id's 4-6
-    function addColDivRow2() { 
-        for (var i = 3; i<=5; i++) { 
-            var divCol = document.createElement('div'); 
-            divCol.setAttribute('id', 'col'+[i]);
-            document.getElementById('row1').appendChild(divCol);
-            document.getElementById('col'+i).style.backgroundColor = arr[Math.floor(Math.random() * 3)];
-        }
-    };
-    //add columns with id's 7-9
-    function addColDivRow3() { 
-        for (var i = 6; i<=8; i++) { 
-            var divCol = document.createElement('div'); 
-            divCol.setAttribute('id', 'col'+[i]);
-            document.getElementById('row2').appendChild(divCol);
-            document.getElementById('col'+i).style.backgroundColor = arr[Math.floor(Math.random() * 3)];
-        }
-    };
-    //add basic css styles to rows
-    function basicStylesRow() {
-        for (var i = 0; i<=2; i++) {
-             var elem = document.getElementById('row'+i); 
-             elem.style.clear = "both";
-        }
-    };
+   
     //add basic css styles to colums
-    function basicStylesCol() {
+    function basicStyles() {
         for (var i = 0; i<=8; i++) {
              var elem = document.getElementById('col'+i); 
              elem.style.border = '#000000 1px solid';
              elem.style.width = '20px';
              elem.style.height = '20px';
-             elem.style.display = "block";
              elem.style.float = "left";
-        }
+            //break row at 3, 6 and 9th column
+            switch(i) {
+                case 3:
+                    elem.style.clear = "both";
+                break;
+                case 6:
+                    elem.style.clear = "both";
+                break;
+                case 9:
+                    elem.style.clear = "both";
+             };
+        };
     };
 
+    //background colours
+    //blue
     function colourBlue(blue) { 
         return blue; 
-    }
-
+    };
+    //red
     function colourRed(red) { 
         return red; 
-    }
-
+    };
+    //green
     function colourGreen(green) { 
         return green; 
-    }
-
+    };
     
-    
-    addRowDiv();
-    addColDivRow1();
-    addColDivRow2();
-    addColDivRow3();
-    basicStylesRow();
-    basicStylesCol();
+    //init functions
+    addColDiv();
+    basicStyles();
 };
-
-//get three colours red, green and blue
-//add the three colours to the grid 
-//each color gets 3 squares
-//randomize the colors to any square (only 3 at a time)
