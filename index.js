070edb161d9f72f49c4dbf9512075fbc8a985a23
flashbax <jkiwtco@gmail.com>
2017-02-21 11:56:16 -0500

    Commit 5

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 window.onload = function() {
     
+    var blue = colourBlue('#4286f4');
+    var red = colourRed('#c40909');
+    var green = colourGreen('#62f441');
+    //add array
+    arr = [blue, red, green];
+    
     //add rows of divs to page with id
     function addRowDiv() { 
         for (var i = 0; i<=2; i++) { 
@@ -11,10 +17,14 @@ window.onload = function() {
 
     //add columns with id's 1-3
     function addColDivRow1() { 
+        
+        
+        //var randommath = Math.floor(Math.random() * 3);
         for (var i = 0; i<=2; i++) { 
             var divCol = document.createElement('div'); 
             divCol.setAttribute('id', 'col'+[i]);
             document.getElementById('row0').appendChild(divCol);
+            document.getElementById('col'+i).style.backgroundColor = arr[Math.floor(Math.random() * 3)];
         }
     };
     //add columns with id's 4-6
@@ -23,6 +33,7 @@ window.onload = function() {
             var divCol = document.createElement('div'); 
             divCol.setAttribute('id', 'col'+[i]);
             document.getElementById('row1').appendChild(divCol);
+            document.getElementById('col'+i).style.backgroundColor = arr[Math.floor(Math.random() * 3)];
         }
     };
     //add columns with id's 7-9
@@ -31,16 +42,17 @@ window.onload = function() {
             var divCol = document.createElement('div'); 
             divCol.setAttribute('id', 'col'+[i]);
             document.getElementById('row2').appendChild(divCol);
+            document.getElementById('col'+i).style.backgroundColor = arr[Math.floor(Math.random() * 3)];
         }
     };
-
+    //add basic css styles to rows
     function basicStylesRow() {
         for (var i = 0; i<=2; i++) {
              var elem = document.getElementById('row'+i); 
              elem.style.clear = "both";
         }
     };
-
+    //add basic css styles to colums
     function basicStylesCol() {
         for (var i = 0; i<=8; i++) {
              var elem = document.getElementById('col'+i); 
@@ -52,6 +64,20 @@ window.onload = function() {
         }
     };
 
+    function colourBlue(blue) { 
+        return blue; 
+    }
+
+    function colourRed(red) { 
+        return red; 
+    }
+
+    function colourGreen(green) { 
+        return green; 
+    }
+
+    
+    
     addRowDiv();
     addColDivRow1();
     addColDivRow2();
@@ -60,5 +86,7 @@ window.onload = function() {
     basicStylesCol();
 };
 
-//Add child divs of rows numbers for each column in eac row
-//Add a function that adds the divs and their ids
+//get three colours red, green and blue
+//add the three colours to the grid 
+//each color gets 3 squares
+//randomize the colors to any square (only 3 at a time)
