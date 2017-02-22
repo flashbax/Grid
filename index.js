3f2f2a6e8f944bcec47244a40bc063e325d7204b
flashbax <jkiwtco@gmail.com>
2017-02-22 12:57:32 -0500

    Commit 8

--- index.js
+++ index.js
@@ -6,20 +6,55 @@ window.onload = function() {
     var blue = '#4286f4'
     var red = '#c40909'
     var green = '#62f441'
+    
+   
+    //red = '(66, 134, 244)';
+    //green = '(98, 244, 65)';
+
     //add array
     arr = [blue, red, green]
-
+    arrbr = [blue, red]
+    arrrg = [red, green]
+    arrbg = [blue, green]
 
     function addColDiv() {     
         for (var i = 0; i<=8; i++) { 
-            var divCol = document.createElement('div'); 
+            var divCol = document.createElement('div');  
             divCol.setAttribute('id', 'col'+i);
             document.getElementById('body').appendChild(divCol);
             document.getElementById('col'+i).style.backgroundColor = arr[Math.floor(Math.random() * 3)];
-            console.log(arr);
+            //console.log(arr);
         };
     };
    
+    function topRowCheck() { 
+        for (var i = 0; i<=2; i++) {
+            var colbg = document.getElementById('col'+i).style.backgroundColor; 
+            //console.log(i);
+            if (i === 0 && colbg === 'rgb(66, 134, 244)') { 
+                    document.getElementById('col1').style.backgroundColor = arrrg[Math.floor(Math.random() * 2)];
+                    document.getElementById('col2').style.backgroundColor = arrrg[Math.floor(Math.random() * 2)];
+            } else { 
+                console.log('Not blue');
+            }
+        }       
+     };
+
+     function middleRowCheck() { 
+        for (var i = 0; i>=2 && i<=5; i++) {
+            var colbg = document.getElementById('col'+i).style.backgroundColor; 
+            console.log(i);
+            // if (i === 0 && colbg === 'rgb(66, 134, 244)') { 
+            //         document.getElementById('col1').style.backgroundColor = arrrg[Math.floor(Math.random() * 2)];
+            //         document.getElementById('col2').style.backgroundColor = arrrg[Math.floor(Math.random() * 2)];
+            // } else { 
+            //     console.log('Not blue');
+            // }
+        }       
+     };
+
+    //document.getElementById('col1').addEventListener("click", topRowCheck());
+
     //add basic css styles to colums
     function basicStyles() {
         for (var i = 0; i<=8; i++) {
@@ -45,4 +80,6 @@ window.onload = function() {
     //init functions
     addColDiv();
     basicStyles();
+    topRowCheck();
+    middleRowCheck();
 };
